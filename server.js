707f94cdeae42a8b33789f8f19ebb9ae22d599da
Martynas-J <Aurimhas@gmail.com>
2023-06-30 07:53:21 +0300

    v1.3

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`JSON serveris paleistas portu ${port}`);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
